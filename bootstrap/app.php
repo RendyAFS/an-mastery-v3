d2aa9688e9f355e78703309481edea97f03fcf9d
IMCPRendy <imcpengwebdev@gmail.com>
2026-01-19 09:18:22 +0700

    update app bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web();
-
         $middleware->validateCsrfTokens(except: ['log-viewer/*']);
         $middleware->alias([
             'role.custom' => \App\Http\Middleware\RoleMiddleware::class,
