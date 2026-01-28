8759423aa9f05a4a8b8030a3c5048b51b7c931d5
IMCPRendy <imcpengwebdev@gmail.com>
2026-01-28 16:52:29 +0700

    going to design datatable and add remove to datatable

---
+++ resources/js/utils/lucide.js
@@ -0,0 +1,9 @@
+export function initLucide(root = document) {
+    if (!window.lucide) return;
+
+    window.lucide.createIcons({
+        attrs: {
+            "stroke-width": 1.75,
+        },
+    });
+}
