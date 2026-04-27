2a226f1b02aec733e2ed9fbeb3d9f35931e73d8a
IMCPRendy <imcpengwebdev@gmail.com>
2026-04-27 08:20:06 +0700

    update minor, and testing makemodule

--- app/Console/Commands/MakeModuleCommand.php
+++ app/Console/Commands/MakeModuleCommand.php
@@ -7,13 +7,15 @@ use Illuminate\Support\Str;
 
 class MakeModuleCommand extends Command
 {
-    protected $signature   = 'make:module {name} {--resource : Generate resource controller instead of basic controller}';
+    protected $signature = 'make:module {name} {--resource : Generate resource controller} {--simple : Generate simple CRUD (modal based)}';
     protected $description = 'Generate module (Controller, Repository, Request)';
 
     public function handle()
     {
         $name = $this->argument('name');
         $isResource = $this->option('resource');
+        $isSimple = $this->option('simple');
+        $kebab = Str::kebab($name);
 
         $studly = Str::studly($name);
 
@@ -40,6 +42,36 @@ class MakeModuleCommand extends Command
             'name' => "{$studly}/Save{$studly}Request",
         ]);
 
+        $baseViewPath = resource_path("views/{$kebab}");
+        $baseJsPath   = resource_path("js/pages/{$kebab}");
+
+        // common files
+        $this->createFile("{$baseViewPath}/index.blade.php");
+        $this->createFile("{$baseViewPath}/form.blade.php");
+        $this->createFile("{$baseJsPath}/list.js");
+
+        if ($isSimple) {
+            $this->createFile("{$baseViewPath}/modal.blade.php");
+        } else {
+            $this->createFile("{$baseViewPath}/create.blade.php");
+            $this->createFile("{$baseViewPath}/edit.blade.php");
+            $this->createFile("{$baseJsPath}/form.js");
+        }
+
         $this->info("✅ Module {$studly} generated successfully!");
+    }
+
+    private function createFile($path, $content = '')
+    {
+        if (!file_exists(dirname($path))) {
+            mkdir(dirname($path), 0755, true);
+        }
+
+        if (!file_exists($path)) {
+            file_put_contents($path, $content);
+            $this->info("📄 Created: {$path}");
+        } else {
+            $this->warn("⚠️ Already exists: {$path}");
+        }
     }
 }
