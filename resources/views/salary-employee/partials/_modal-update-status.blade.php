a040498b08ff2d25964b2fc56079b6d633de4dec
IMCPRendy <imcpengwebdev@gmail.com>
2026-07-27 16:53:20 +0700

    feat: add update status modal component for employee salary records

--- resources/views/salary-employee/partials/_modal-update-status.blade.php
+++ resources/views/salary-employee/partials/_modal-update-status.blade.php
@@ -1,5 +1,5 @@
 <div id="modal-update-status-salary"
-    class="hs-overlay hidden size-full fixed top-0 start-0 z-80 overflow-x-hidden overflow-y-auto">
+    class="hs-overlay hidden size-full fixed top-0 inset-s-0 z-80 overflow-x-hidden overflow-y-auto">
     <div
         class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500
         mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
