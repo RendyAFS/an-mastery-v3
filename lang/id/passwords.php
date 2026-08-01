869f4f8f566e1f64a536dbc070b8c259c0521fd7
IMCPRendy <imcpengwebdev@gmail.com>
2026-08-01 07:58:26 +0700

    feat: add English and Indonesian localization files for authentication, validation, and pagination

---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reset Password Bahasa Garis
+    |--------------------------------------------------------------------------
+    |
+    | Bahasa baris berikut adalah baris default yang cocok dengan alasan
+    | yang diberikan oleh password broker untuk percobaan reset password
+    | seperti kegagalan karena password / token reset tidak valid.
+    |
+    */
+
+    'reset'     => 'Password Anda telah berhasil di-reset.',
+    'sent'      => 'Kami telah mengirimkan tautan reset password Anda melalui email.',
+    'throttled' => 'Mohon tunggu sebelum mencoba lagi.',
+    'token'     => 'Token reset password ini tidak valid.',
+    'user'      => "Kami tidak dapat menemukan pengguna dengan alamat email ini.",
+
+];
