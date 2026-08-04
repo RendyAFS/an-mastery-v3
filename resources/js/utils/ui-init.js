4b1d69087f5b166d33c8d77abfff1944c125731e
IMCPRendy <imcpengwebdev@gmail.com>
2026-08-04 15:15:37 +0700

    feat: implement centralized UI component initialization utility

--- resources/js/utils/ui-init.js
+++ resources/js/utils/ui-init.js
@@ -3,7 +3,34 @@ import RupiahInput from "./rupiah-input";
 
 export default function initUi() {
     requestAnimationFrame(() => {
-        window.HSStaticMethods?.autoInit();
+        window.HSStaticMethods?.autoInit([
+            "accessibility-observer",
+            "accordion",
+            "carousel",
+            "collapse",
+            "combo-box",
+            "copy-markup",
+            "dropdown",
+            "file-upload",
+            "input-number",
+            "layout-splitter",
+            "overlay",
+            "pin-input",
+            "range-slider",
+            "remove-element",
+            "scroll-nav",
+            "scrollspy",
+            "select",
+            "stepper",
+            "strong-password",
+            "tabs",
+            "textarea-auto-height",
+            "theme-switch",
+            "toggle-count",
+            "toggle-password",
+            "tooltip",
+            "tree-view",
+        ]);
         initLucide();
         RupiahInput.init();
     });
