9bf6f19e42dd3022069edf0e06c9782cba551c1e
RendyAFS <arendy724@gmail.com>
2026-08-08 06:43:00 +0700

    feat: implement Progressive Web App (PWA) support using laravel-pwa package

---
+++ config/pwa.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Would you like the install button to appear on all pages?
+      Set true/false
+    |--------------------------------------------------------------------------
+    */
+
+    'install-button' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | PWA Manifest Configuration
+    |--------------------------------------------------------------------------
+    |  php artisan erag:update-manifest
+    */
+
+    'manifest' => [
+        'name' => 'An Mastery',
+        'short_name' => 'ANMT',
+        'background_color' => '#6d9886',
+        'display' => 'fullscreen',
+        'description' => 'An Mastery System ',
+        'theme_color' => '#6d9886',
+        'icons' => [
+            [
+                'src' => 'logo.png',
+                'sizes' => '512x512',
+                'type' => 'image/png',
+            ],
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Debug Configuration
+    |--------------------------------------------------------------------------
+    | Toggles the application's debug mode based on the environment variable
+    */
+
+    'debug' => env('APP_DEBUG', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Livewire Integration
+    |--------------------------------------------------------------------------
+    | Set to true if you're using Livewire in your application to enable
+    | Livewire-specific PWA optimizations or features.
+    */
+
+    'livewire-app' => false,
+];
