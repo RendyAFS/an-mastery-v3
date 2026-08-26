a73e61f171bda6f3abc00a12006480f6f6ede900
IMCPRendy <imcpengwebdev@gmail.com>
2026-08-26 08:15:33 +0700

    feat: add app configuration file and bump application version to 1.1.2

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'AN Mastery'),
 
-    'version' => env('APP_VERSION', '1.1.1'),
+    'version' => env('APP_VERSION', '1.1.2'),
 
     /*
     |--------------------------------------------------------------------------
