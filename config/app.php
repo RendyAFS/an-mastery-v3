dcb1485e0d275ab2d1e1fd0c07288cd73fca3926
IMCPRendy <imcpengwebdev@gmail.com>
2026-08-27 09:24:02 +0700

    chore: bump version to 1.1.3 and initialize app configuration file

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'AN Mastery'),
 
-    'version' => env('APP_VERSION', '1.1.2'),
+    'version' => env('APP_VERSION', '1.1.3'),
 
     /*
     |--------------------------------------------------------------------------
