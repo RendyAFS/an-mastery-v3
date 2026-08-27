2b702662f7bdea3beab2d2008607754ada072d08
IMCPRendy <imcpengwebdev@gmail.com>
2026-08-27 09:34:36 +0700

    feat: include resources/js/utils directory in Vite build paths

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
                 "resources/js/app.js",
                 ...glob.sync([
                     "resources/js/pages/**/*.js",
+                    "resources/js/utils/*.js",
                 ]),
             ],
             refresh: true,
