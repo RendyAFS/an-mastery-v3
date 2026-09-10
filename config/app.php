72c584ba94d6fd3de826fa8899345d60a0015023
IMCPRendy <imcpengwebdev@gmail.com>
2026-09-10 09:37:28 +0700

    chore: add application configuration file and update version to 1.1.5

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'AN Mastery'),
 
-    'version' => env('APP_VERSION', '1.1.4'),
+    'version' => env('APP_VERSION', '1.1.5'),
 
     /*
     |--------------------------------------------------------------------------
