1af6eab4f6b7946dfca314ee7c7571c73f6df5fc
IMCPRendy <imcpengwebdev@gmail.com>
2026-09-14 09:19:50 +0700

    chore: add application configuration file and bump version to 1.1.8

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'AN Mastery'),
 
-    'version' => env('APP_VERSION', '1.1.7'),
+    'version' => env('APP_VERSION', '1.1.8'),
 
     /*
     |--------------------------------------------------------------------------
