b1c72cc72c5136b78267018452727e49894e963d
RendyAFS <arendy724@gmail.com>
2026-09-21 18:06:06 +0700

    feat: add application configuration file and update version to 1.2.0

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'AN Mastery'),
 
-    'version' => env('APP_VERSION', '1.1.9'),
+    'version' => env('APP_VERSION', '1.2.0'),
 
     /*
     |--------------------------------------------------------------------------
